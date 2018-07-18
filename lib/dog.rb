f627ce2d59b031ffc5c1bb24ab79df102d49ee65
Scott Ungchusri <animoogle504@gmail.com>
2018-07-18 04:06:11 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog 
+  #I am dog. Boof
+end
+
+#clifford = Dog.new  jumped the gun and didn't read the instructions. got excited at dog.
+#thunderDog = Dog.new
+#hershel = Dog.new
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  #I am person. I need help.
+end
+
+adele_goldberg = Person.new 
+alan_kay = Person.new
